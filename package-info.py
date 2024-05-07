2c6bcdd76812dff829e276e87ff492986a6c0b00
V.Kotliar <victor.kotlyar@ihep.ru>
2024-05-07 19:07:38 +0530

    Fix build container version

--- package-info.py
+++ package-info.py
@@ -17,7 +17,7 @@
            'web': []},
  'name': 'webups',
  'title': ' webups plugin for check_mk 2',
- 'version': '2.1',
- 'version.min_required': '2.0.0p1',
- 'version.packaged': '2.0.0p5',
+ 'version': '1.0',
+ 'version.min_required': '2.2.0p11',
+ 'version.packaged': '2.2.0p11',
  'version.usable_until': None}
